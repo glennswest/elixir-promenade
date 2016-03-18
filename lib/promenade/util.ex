b565a1cb1a5dba04d883dc4b2a004a907b5620ee
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2016-03-18 14:25:58 -0700

    Remove now-unused Util module.

--- lib/promenade/util.ex
+++
@@ -1,9 +0,0 @@
-
-defmodule Promenade.Util do
-  
-  def reraise_prefixed(exception, prefix_message) do
-    type = exception.__struct__ |> inspect
-    message = "#{prefix_message}\n(#{type}) #{Exception.message(exception)}"
-    reraise(message, System.stacktrace)
-  end
-end
