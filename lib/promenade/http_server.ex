8c46fc5adbbcd315b20e7a0cb2829ae5809298ca
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2016-12-28 09:08:29 -0800

    Add tests for HttpServer module

--- lib/promenade/http_server.ex
+++ lib/promenade/http_server.ex
@@ -3,6 +3,9 @@ defmodule Promenade.HttpServer do
   require Logger
   import Plug.Conn
   
+  alias Promenade.Registry
+  alias Promenade.TextFormat
+  
   def port, do: Application.fetch_env!(:promenade, :http_port)
   def start_link(name, opts) do
     # TODO: make this actually linked
@@ -11,19 +14,21 @@ defmodule Promenade.HttpServer do
     Plug.Adapters.Cowboy.http(__MODULE__, opts, port: port)
   end
   
-  def init(opts) do
-    opts
-  end
+  def init(opts), do: opts
   
   def call(conn, opts) do
     text =
       opts
       |> Keyword.fetch!(:tables)
-      |> Promenade.Registry.data
-      |> Promenade.TextFormat.snapshot
+      |> Registry.data
+      |> TextFormat.snapshot
     
+    conn |> respond(200, text)
+  end
+  
+  defp respond(conn, code, body) do
     conn
     |> put_resp_content_type("text/plain")
-    |> send_resp(200, text)
+    |> send_resp(code, body)
   end
 end
---
+++ test/promenade/http_server_test.exs
@@ -0,0 +1,36 @@
+
+defmodule Promenade.HttpServerTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+  
+  alias Promenade.HttpServer
+  alias Promenade.Registry
+  alias Promenade.TextFormat
+  
+  def registry do
+    {:error, {:already_started, pid}} = Registry.start_link(Registry, [])
+    
+    pid
+  end
+  
+  def tables, do: registry |> Registry.get_tables
+  
+  def call(method, path) do
+    conn(method, path)
+    |> HttpServer.call(registry: registry, tables: tables)
+  end
+  
+  test "/metrics" do
+    registry |> Registry.handle_metrics [
+      {:gauge, "foo", 88.8, %{ "x" => "XXX" }},
+    ]
+    
+    expected_body = tables |> Registry.data |> TextFormat.snapshot
+    
+    conn = call(:get, "/metrics")
+    
+    assert conn.state     == :sent
+    assert conn.status    == 200
+    assert conn.resp_body == expected_body
+  end
+end
