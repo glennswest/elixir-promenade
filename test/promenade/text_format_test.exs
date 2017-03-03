a67ec3061928fbe8565139cfeceed4f78cc07a1b
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2017-03-03 15:30:37 -0800

    Improve indentation in TextFormatTest.

--- test/promenade/text_format_test.exs
+++ test/promenade/text_format_test.exs
@@ -37,39 +37,39 @@ defmodule Promenade.TextFormatTest do
     }
     
     assert TextFormat.snapshot(data) ==
-"""
-# TYPE foo gauge
-foo{x="XXX"} 77.7
-foo{y="YYY"} 44.4
-
-# TYPE foo2 gauge
-foo2{x="XXX",y="YYY"} 33.3
-
-# TYPE bar counter
-bar{x="XXX"} 222
-bar{y="YYY"} 33
-
-# TYPE bar2 counter
-bar2{x="XXX",y="YYY"} 111
-
-# TYPE baz summary
-baz{quantile="0.5",x="XXX"} 5.5
-baz{quantile="0.9",x="XXX"} 5.5
-baz{quantile="0.99",x="XXX"} 5.5
-baz_sum{x="XXX"} 5.5
-baz_count{x="XXX"} 1
-baz{quantile="0.5",y="YYY"} 6.6
-baz{quantile="0.9",y="YYY"} 6.6
-baz{quantile="0.99",y="YYY"} 6.6
-baz_sum{y="YYY"} 6.6
-baz_count{y="YYY"} 1
-
-# TYPE baz2 summary
-baz2{quantile="0.5",x="XXX",y="YYY"} 3.3
-baz2{quantile="0.9",x="XXX",y="YYY"} 3.3
-baz2{quantile="0.99",x="XXX",y="YYY"} 3.3
-baz2_sum{x="XXX",y="YYY"} 3.3
-baz2_count{x="XXX",y="YYY"} 1
-"""
+      """
+      # TYPE foo gauge
+      foo{x="XXX"} 77.7
+      foo{y="YYY"} 44.4
+      
+      # TYPE foo2 gauge
+      foo2{x="XXX",y="YYY"} 33.3
+      
+      # TYPE bar counter
+      bar{x="XXX"} 222
+      bar{y="YYY"} 33
+      
+      # TYPE bar2 counter
+      bar2{x="XXX",y="YYY"} 111
+      
+      # TYPE baz summary
+      baz{quantile="0.5",x="XXX"} 5.5
+      baz{quantile="0.9",x="XXX"} 5.5
+      baz{quantile="0.99",x="XXX"} 5.5
+      baz_sum{x="XXX"} 5.5
+      baz_count{x="XXX"} 1
+      baz{quantile="0.5",y="YYY"} 6.6
+      baz{quantile="0.9",y="YYY"} 6.6
+      baz{quantile="0.99",y="YYY"} 6.6
+      baz_sum{y="YYY"} 6.6
+      baz_count{y="YYY"} 1
+      
+      # TYPE baz2 summary
+      baz2{quantile="0.5",x="XXX",y="YYY"} 3.3
+      baz2{quantile="0.9",x="XXX",y="YYY"} 3.3
+      baz2{quantile="0.99",x="XXX",y="YYY"} 3.3
+      baz2_sum{x="XXX",y="YYY"} 3.3
+      baz2_count{x="XXX",y="YYY"} 1
+      """
   end
 end
