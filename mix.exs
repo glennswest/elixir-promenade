a7e48e56025ec7e7e85739c0dae36b9d8628d2a0
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2017-03-07 13:14:25 -0800

    Version 0.4.0.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Promenade.Mixfile do
   def project do
     [
       app: :promenade,
-      version: "0.3.0",
+      version: "0.4.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
